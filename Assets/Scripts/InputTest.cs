de96dde98b0c425577d75117f89e54790b89e50d
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-01-08 09:54:20 -0700

    Assets, Packages, ProjectSettings

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputTest : MonoBehaviour
+{
+    private PlayerInputActions _testActions;
+
+     void Awake()
+     {
+         _testActions = new PlayerInputActions(); //we created an object of the class PlayerInputActions
+         _testActions.Enable(); //we turn it on to listen to key inputs
+     }
+
+     void OnEnable()
+     {
+         _testActions.Player.Jump.performed += Jump;
+     }
+
+     void OnDisable()
+    {
+        _testActions.Player.Jump.performed -= Jump;
+    }
+
+    void Jump(InputAction.CallbackContext ctx)
+    {
+        Debug.Log("Jump");
+    }
+}
