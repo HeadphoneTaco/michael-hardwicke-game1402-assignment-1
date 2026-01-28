a42bc2f022fdfda9cc5fbf30d4e9ebea742db1ca
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-01-28 15:57:31 -0700

    Start Player Footstep script

---
+++ Assets/Scripts/PlayerFootStep.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class PlayerFootStep : MonoBehaviour
+{
+
+    [SerializeField] private ParticleSystem footStepEffect;
+    
+    [SerializeField] private string targetTag = "ground";
+
+
+    private void OnCollisionEnter2D(Collision2D col)
+    
+    {
+
+        if (col.gameObject.CompareTag(targetTag))
+        {
+            footStepEffect.Play();
+        }
+
+    }
+}
