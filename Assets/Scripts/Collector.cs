53ea649567333f7db0b083f74178da19b31e697b
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-01-28 18:00:00 -0700

    Moving Platform

--- Assets/Scripts/Collector.cs
+++ Assets/Scripts/Collector.cs
@@ -4,6 +4,13 @@ public class Collector : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
-   
+        ICollectable otherCollectable = other.GetComponent<ICollectable>();
+
+        if (otherCollectable != null)
+        {
+            otherCollectable.OnCollect();
+        }
+        
+        
     }
 }
