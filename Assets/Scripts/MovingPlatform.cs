ba4faff8736492784e54ca494918c72115802763
HeadphoneTaco <michael.hardwicke92@gmail.com>
2026-02-03 17:49:03 -0700

    Platforms are yeeting themselves

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -4,8 +4,8 @@ public class MovingPlatform : MonoBehaviour
 {
     [SerializeField] private float cycleTime = 5f;
 
-    [SerializeField] private Transform pointA;
-    [SerializeField] private Transform pointB;
+    [SerializeField] private Transform point1;
+    [SerializeField] private Transform point2;
     
     private float _currentTime;
     private float _speed = 1f;
@@ -14,12 +14,12 @@ public class MovingPlatform : MonoBehaviour
     {
         _currentTime += _speed * Time.deltaTime;
 
-        if (_currentTime > cycleTime) _speed = -1f;
-        if (_currentTime < 0f) _speed = 1f;
+        if (_currentTime < cycleTime) _speed = -1f;
+        if (_currentTime > 0f) _speed = 1f;
 
         float t = _currentTime / cycleTime;
         
-        transform.position = Vector3.Lerp(pointA.position, pointB.position, t);
+        transform.position = Vector3.Lerp(point1.position, point2.position, t);
 
     }
 }
